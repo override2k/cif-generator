9ab209e9a8267a1c34574b2d283131b80e320a95
Fernando Robledo <fernando.robledo@opinno.com>
2018-03-23 23:25:20 +0100

    Added style-ci

--- tests/CifTest.php
+++ tests/CifTest.php
@@ -10,14 +10,12 @@ use Overdesign\CifGenerator\Cif;
  */
 class CifTest extends PHPUnit_Framework_TestCase
 {
-
     public function testCif()
     {
         for ($i = 1; $i <= 1000; $i++) {
             $cif = Cif::generate();
             $this->assertTrue(IsoCodes\Cif::validate($cif), sprintf('Generated CIF: %s is not valid', $cif));
         }
-
     }
 
     public function testGenerateCif()
